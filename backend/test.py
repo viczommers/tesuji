57906d215f339f9c0b0b8b4897b3a753fcb79fd9
viczommers <victor.zommers@icloud.com>
2025-04-12 23:31:21 +0100

    wip: test to func

--- backend/test.py
+++ backend/test.py
@@ -39,34 +39,36 @@ prompt = (
     f"Suggest the best three suitable doctors for the user and include a brief justification"
     f"Return the result as a json object with the fields 'Name', 'Specialty', 'Price', 'Availability', 'Rating', and 'Justification', each populated accordingly"
 )
-plan = portia.plan(prompt)
-#print(f"{plan.model_dump_json(indent=2)}")
 
-# Run the plan
-plan_run = portia.run_plan(plan)
 
-while plan_run.state == PlanRunState.NEED_CLARIFICATION:
-    # If clarifications are needed, resolve them before resuming the plan run
-    for clarification in plan_run.get_outstanding_clarifications():
-        # Usual handling of Input and Multiple Choice clarifications
-        if isinstance(clarification, (InputClarification, MultipleChoiceClarification)):
-            print(f"{clarification.user_guidance}")
-            user_input = input("Please enter a value:\n" 
-                            + (("\n".join(clarification.options) + "\n") if "options" in clarification else ""))
-            plan_run = portia.resolve_clarification(clarification, user_input, plan_run)
+def book_appointment():
+    plan = portia.plan(prompt)
+    #print(f"{plan.model_dump_json(indent=2)}")
+    # Run the plan
+    plan_run = portia.run_plan(plan)
+
+    while plan_run.state == PlanRunState.NEED_CLARIFICATION:
+        # If clarifications are needed, resolve them before resuming the plan run
+        for clarification in plan_run.get_outstanding_clarifications():
+            # Usual handling of Input and Multiple Choice clarifications
+            if isinstance(clarification, (InputClarification, MultipleChoiceClarification)):
+                print(f"{clarification.user_guidance}")
+                user_input = input("Please enter a value:\n" 
+                                + (("\n".join(clarification.options) + "\n") if "options" in clarification else ""))
+                plan_run = portia.resolve_clarification(clarification, user_input, plan_run)
+            
+            # Handling of Action clarifications
+            if isinstance(clarification, ActionClarification):
+                print(f"{clarification.user_guidance} -- Please click on the link below to proceed.")
+                print(clarification.action_url)
+                plan_run = portia.wait_for_ready(plan_run)
+
+        # Once clarifications are resolved, resume the plan run
+        plan_run = portia.resume(plan_run)
         
-        # Handling of Action clarifications
-        if isinstance(clarification, ActionClarification):
-            print(f"{clarification.user_guidance} -- Please click on the link below to proceed.")
-            print(clarification.action_url)
-            plan_run = portia.wait_for_ready(plan_run)
 
-    # Once clarifications are resolved, resume the plan run
-    plan_run = portia.resume(plan_run)
-    
+    # Serialise into JSON and print the output
+    #print(f"{plan_run.model_dump_json(indent=2)}")
 
-# Serialise into JSON and print the output
-#print(f"{plan_run.model_dump_json(indent=2)}")
-
-final_output = plan_run.outputs.final_output.get_value()
-print(final_output)
+    final_output = plan_run.outputs.final_output.get_value()
+    return final_output
