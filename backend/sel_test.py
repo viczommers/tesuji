e466a802a9ed11da7f38c1885b8b3b05869a7067
JUSTIN <ZLAC478@live.rhul.ac.uk>
2025-04-12 17:11:01 +0100

    Add web scraping for doctor information

--- backend/sel_test.py
+++ backend/sel_test.py
@@ -4,10 +4,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
+import json
 
 
-postcode="NW1 8DU"
-specialty="cardiologist"
+postcode="TW20 9LQ"
+specialty="Cardiology"
 procedure=""
 
 driver = webdriver.Chrome()
@@ -16,7 +17,7 @@ url = "https://www.phin.org.uk/"
 
 driver.get(url)
 
-wait = WebDriverWait(driver, 5)
+wait = WebDriverWait(driver, 10)
 cookie_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".button.white.svelte-1j7atkd")))
 cookie_button.click()
 postcode_input = driver.find_element(By.NAME, "location-input")
@@ -33,3 +34,29 @@ submission_button = driver.find_element(By.CSS_SELECTOR, ".search-button.svelte-
 
 submission_button.click()
 time.sleep(2)
+
+doctors_list = driver.find_elements(By.CSS_SELECTOR, ".search-result.svelte-gx346u")
+
+#Look through each doctor and get their info, adding their data as an object to a list of doctors' data
+doctors = []
+
+for doctor in doctors_list[:10]:
+    try:
+        doctor_name = doctor.find_element(By.CSS_SELECTOR, ".name.svelte-1n2ywad").text
+        doctor_specialty = doctor.find_element(By.CSS_SELECTOR, ".value.svelte-ivi3vh").text
+        doctor_rating = doctor.find_element(By.CSS_SELECTOR, ".value.svelte-vnlxoq").text
+        doctor_distance = doctor.find_element(By.XPATH, "(//td[@data-description='Distance'])").text
+        doctor_price = doctor.find_element(By.XPATH, "(//div[@data-description='Fee']//div)").text
+        doctor_data = {
+            'name': doctor_name,
+            'specialty': doctor_specialty,
+            'rating': doctor_rating,
+            'distance': doctor_distance,
+            'price': doctor_price
+        }
+        doctors.append(doctor_data)
+    except: pass
+
+time.sleep(1)
+
+print([doctor for doctor in doctors])
